43c9ba4f7a2ba369718c09c1d78bf4d5ee29c172
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-11-07 09:19:28 +0200

    Add AnnotatedBeanDefinitionReader

---
+++ src/main/java/com/breskul/bring/context/annotation/AnnotatedBeanDefinitionReader.java
@@ -0,0 +1,8 @@
+package com.breskul.bring.context.annotation;
+
+public class AnnotatedBeanDefinitionReader {
+
+    private <T> void registerBean(Class<T> beanClass, String beanName) {
+        // TODO: write registerBean method logic
+    }
+}

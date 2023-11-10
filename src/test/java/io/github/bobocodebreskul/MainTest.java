cbaea75b2eaa9dba736cc3ed6394881b5e873dd6
mykola.filimonov <filimonovkolya97@gmail.com>
2023-11-10 12:08:24 +0200

    add fix paths

---
+++ src/test/java/io/github/bobocodebreskul/MainTest.java
@@ -0,0 +1,11 @@
+package io.github.bobocodebreskul;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+  @org.junit.jupiter.api.Test
+  void main() {
+    assertTrue(true);
+  }
+}

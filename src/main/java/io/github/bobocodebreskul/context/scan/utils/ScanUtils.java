e2f57c87769524f76b4f33f3fa637a9dd5c3869b
Oleksandr.Karpachov <xalexandrx9@gmail.com>
2023-11-13 19:23:53 +0200

    us #16 add java docs.

--- src/main/java/io/github/bobocodebreskul/context/scan/utils/ScanUtils.java
+++ src/main/java/io/github/bobocodebreskul/context/scan/utils/ScanUtils.java
@@ -4,17 +4,34 @@ import java.lang.annotation.Annotation;
 import java.util.Set;
 import java.util.function.Predicate;
 
-// TODO: docs
+/**
+ * Util class for searching classes in packages.
+ */
 public interface ScanUtils {
     /**
-     * Search all classes in given package except.
-     * @param packagePath - package name
-     * @return List of
+     * Search all classes, (except enums and records) in given package including nested packages.
+     * @param packagePath - root package for search.
+     * @return Set of found classes
      */
-    // TODO: docs
     Set<Class<?>> searchAllClasses(String packagePath);
-    // TODO: docs
-    Set<Class<?>> searchClassesByAnnotationRecursively(String packagePath, Class<? extends Annotation> annotation);
-    // TODO: docs
-    Set<Class<?>> searchClassesByFilter(String packagePath, Predicate<Class<?>> filter);
+
+    /**
+     * Search classes which has given annotation or annotations which has given annotation inside.
+     * For example if @Service annotation has @Component annotation, and we're searching
+     * by @Component we also will find all classes with @Service service annotation.
+     * !Note: given annotation must have @Retention(RetentionPolicy.RUNTIME)
+     *
+     * @param packagePathPrefix root package for search.
+     * @param annotation target annotation.
+     * @return Set of all classes with given annotation.
+     */
+    Set<Class<?>> searchClassesByAnnotationRecursively(String packagePathPrefix, Class<? extends Annotation> annotation);
+
+    /**
+     * Search classes by filter in given package including nested packages.
+     * @param packagePathPrefix root package for search.
+     * @param filter filter predicate which applies on found classes
+     * @return Set of all classes which pass the filter.
+     */
+    Set<Class<?>> searchClassesByFilter(String packagePathPrefix, Predicate<Class<?>> filter);
 }

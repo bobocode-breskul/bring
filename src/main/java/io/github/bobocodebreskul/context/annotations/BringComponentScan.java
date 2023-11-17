f44dcfa0ad5628657d5eafe3c5520d97a45878b4
oleg.iakovenko <iakovenkoua@gmail.com>
2023-11-17 11:59:39 +0200

    temp

--- src/main/java/io/github/bobocodebreskul/context/annotations/BringComponentScan.java
+++ src/main/java/io/github/bobocodebreskul/context/annotations/BringComponentScan.java
@@ -1,5 +1,16 @@
 package io.github.bobocodebreskul.context.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface BringComponentScan {
 
+  /**
+   *
+   */
+  String[] basePackages() default {};
 }

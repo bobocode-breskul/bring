3924ac855ad8d498f109c077bcb21f4512cd86e3
oleg.iakovenko <iakovenkoua@gmail.com>
2023-11-23 18:49:13 +0200

    add docs

--- src/main/java/io/github/bobocodebreskul/context/annotations/Scope.java
+++ src/main/java/io/github/bobocodebreskul/context/annotations/Scope.java
@@ -5,7 +5,38 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// TODO add java docs
+/**
+ * Indicates the scope to use for the annotated component or bean.
+ * <p>
+ * This annotation is used to define the lifecycle and visibility of a bean in the context. It
+ * allows specifying the desired scope for a particular component or bean.
+ * <p>
+ * The {@code @Scope} annotation can be applied to classes marked as Bring components or beans. The
+ * specified scope determines how the Bring container manages the instance of the annotated
+ * component.
+ * <p>
+ * If the {@code value} attribute is not explicitly set, the default scope is assumed to be
+ * singleton.
+ * <p>
+ * Possible values for the {@code value} attribute:
+ * <ul>
+ *   <li>{@code ""} (empty string): Singleton scope (default)</li>
+ *   <li>{@code "singleton"}: Singleton scope</li>
+ *   <li>{@code "prototype"}: Prototype scope</li>
+ * </ul>
+ * <p>
+ * Example usage:
+ * <pre>
+ * {@code
+ * @Scope("prototype")
+ * public class MyPrototypeBean {
+ *     // ...
+ * }
+ * }
+ * </pre>
+ *
+ * @since 1.0
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Scope {

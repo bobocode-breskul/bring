d7e359b202745beb6a36d4be01b7e8dd5b1ed36f
Aleksandr Karpachov <47796275+AlexandrKarpachov@users.noreply.github.com>
2023-11-24 09:00:53 +0200

    us #84 filter annotation. (#86)

--- src/main/java/io/github/bobocodebreskul/context/scan/RecursiveClassPathAnnotatedBeanScanner.java
+++ src/main/java/io/github/bobocodebreskul/context/scan/RecursiveClassPathAnnotatedBeanScanner.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Implementation of {@link ClassPathAnnotatedBeanScanner} to recursively find all bean definitions
@@ -42,7 +43,10 @@ public class RecursiveClassPathAnnotatedBeanScanner implements ClassPathAnnotate
       String scanPackage = remainingScanPackages.poll();
       processedScanPackages.add(scanPackage);
 
-      Set<Class<?>> foundClasses = scanSingle(scanPackage);
+      Set<Class<?>> foundClasses = scanSingle(scanPackage).stream()
+        .filter(clazz -> !clazz.isAnnotation())
+        .collect(Collectors.toSet());
+
       foundClasses.forEach(beanDefinitionReader::registerBean);
       // TODO: implement package scan found on discovered configurations
       // recursively find all bean from other found configurations

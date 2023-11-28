5c17702aeee04a389135b4904ced016050e0f98c
RomanPryshchepa <roman.pryshchepa@gmail.com>
2023-11-28 21:42:53 +0100

    #69 add methods to BringResponse, add logs and tests

--- src/main/java/io/github/bobocodebreskul/server/BringResponse.java
+++ src/main/java/io/github/bobocodebreskul/server/BringResponse.java
@@ -1,11 +1,15 @@
 package io.github.bobocodebreskul.server;
 
+import static java.util.Objects.isNull;
+
+import io.github.bobocodebreskul.server.enums.ResponseStatus;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import javax.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
 
-/**
+/*
  * (done) body (get, set)
  * (done) status (get, set)
  * (in progress) headers()
@@ -14,37 +18,72 @@ import lombok.extern.slf4j.Slf4j;
  * response.builder().setBody(body).setStatus(200).build();
  */
 
+/**
+ * todo write java doc
+ * @param <T>
+ */
 @Slf4j
 public class BringResponse<T> {
 
+  static final String COOKIE = "Cookie";
+  static final String HEADER_NAME_SHOULD_NOT_BE_NULL = "Header name should not be null.";
+  static final String HEADER_VALUE_SHOULD_NOT_BE_NULL = "Header value should not be null.";
+  static final String COOKIE_NAME_SHOULD_NOT_BE_NULL = "Cookie name should not be null.";
+  static final String COOKIE_VALUE_SHOULD_NOT_BE_NULL = "Cookie value should not be null.";
 
-  private final Map<String, String> headers = new HashMap<>();
+  private Map<String, String> headers = new HashMap<>();
 
   private final Map<String, String> cookies = new HashMap<>();
   T body;
-  Integer status;
+  ResponseStatus status;
 
   public BringResponse(T body) {
     this.body = body;
+    this.status = ResponseStatus.OK;
   }
 
+  public BringResponse(ResponseStatus status) {
+    this(null, new HashMap<>(), status);
+  }
+
+  public BringResponse(@Nullable T body, @Nullable Map<String, String> headers, ResponseStatus status) {
+    // todo map headers to lowercase and test it.
+    this.headers = isNull(headers) ? new HashMap<>() : headers;
+    this.body = body;
+    if (isNull(status)) {
+      log.error("HttpStatus must not be null");
+      throw new IllegalArgumentException("HttpStatus must not be null");
+    }
+    this.status = status;
+  }
+
+  /**
+   * Method adds header to {@link BringResponse}
+   * @param headerName header name
+   * @param headerValue header value
+   */
   public void addHeader(String headerName, String headerValue) {
     log.info("Add header with name='{}' and value='{}'.", headerName, headerValue);
-    if (Objects.isNull(headerName)) {
+    if (isNull(headerName)) {
       log.error("Adding header failed. Header name should not be null.");
-      throw new IllegalArgumentException("Header key should not be null.");
+      throw new IllegalArgumentException(HEADER_NAME_SHOULD_NOT_BE_NULL);
     }
-    if (Objects.isNull(headerValue)) {
+    if (isNull(headerValue)) {
       log.error("Adding header failed. Header value should not be null.");
-      throw new IllegalArgumentException("Header value should not be null.");
+      throw new IllegalArgumentException(HEADER_VALUE_SHOULD_NOT_BE_NULL);
     }
-    this.headers.put(headerName, headerValue);
+    this.headers.put(headerName.toLowerCase(), headerValue);
     log.debug("Header with name='{}' and value='{}' was added.", headerName, headerValue);
   }
 
+  /**
+   * Returns header value by name.
+   * @param headerName header name.
+   * @return header value.
+   */
   public String getHeader(String headerName) {
     log.info("Get header for headerName='{}'.", headerName);
-    if (this.headers.containsKey(headerName)) {
+    if (this.headers.containsKey(headerName.toLowerCase())) {
       String headerValue = this.headers.get(headerName);
       log.info("Header value for header name '{}' is '{}'.", headerName, headerValue);
       return headerValue;
@@ -53,6 +92,93 @@ public class BringResponse<T> {
     return "";
   }
 
+  /**
+   * Removes header from {@link BringResponse}
+   * @param headerName header name.
+   */
+  public void removeHeader(String headerName) {
+    log.info("Remove header for headerName='{}'.", headerName);
+    if (isNull(headerName)) {
+      log.error("Adding header failed. Header name should not be null.");
+      throw new IllegalArgumentException(HEADER_NAME_SHOULD_NOT_BE_NULL);
+    }
+    if (this.headers.containsKey(headerName.toLowerCase())) {
+      if (headerName.equalsIgnoreCase(COOKIE)) {
+        cookies.clear();
+      }
+      String headerValue = headers.get(headerName.toLowerCase());
+      this.headers.remove(headerName.toLowerCase());
+      log.info("Header '{}: {}' was removed.", headerName, headerValue);
+    }
+    log.warn("Removing failed. Header for name='{}' not found.", headerName);
+  }
+
+  /**
+   * Method for adding cookies to {@link BringResponse}
+   * @param cookieName cookie name.
+   * @param cookieValue cookie value.
+   */
+  public void addCookie(String cookieName, String cookieValue) {
+    log.info("Add cookie with name='{}' and value='{}'.", cookieName, cookieValue);
+    if (isNull(cookieName)) {
+      log.error("Adding cookie failed. Cookie name should not be null.");
+      throw new IllegalArgumentException(COOKIE_NAME_SHOULD_NOT_BE_NULL);
+    }
+    if (isNull(cookieValue)) {
+      log.error("Adding cookie failed. Cookie value should not be null.");
+      throw new IllegalArgumentException(COOKIE_VALUE_SHOULD_NOT_BE_NULL);
+    }
+    this.cookies.put(cookieName, cookieValue.toLowerCase());
+    log.debug("Cookie with name='{}' and value='{}' was added.", cookieName, cookieValue);
+    String cookiesAsString = getCookiesAsString();
+    this.headers.put(COOKIE, cookiesAsString);
+    log.debug("Cookies '{}' was added to headers.", cookiesAsString);
+  }
+
+  /**
+   * Returns cookie value from {@link BringResponse}.
+   * @param cookieName cookie name.
+   * @return cookie value.
+   */
+  public String getCookie(String cookieName) {
+    log.info("Get cookie for cookieName='{}'.", cookieName);
+    if (this.cookies.containsKey(cookieName.toLowerCase())) {
+      String cookieValue = this.cookies.get(cookieName.toLowerCase());
+      log.info("Cookie value for cookie name '{}' is '{}'.", cookieName, cookieValue);
+      return cookieValue;
+    }
+    log.warn("Cookie for name='{}' not found.", cookieName);
+    return "";
+  }
+
+  /**
+   * Removes cookie from {@link BringResponse} by name.
+   * @param cookieName removed cookie name.
+   */
+  public void removeCookie(String cookieName) {
+    log.info("Remove cookie for cookieName='{}'.", cookieName);
+    if (isNull(cookieName)) {
+      log.error("Adding cookie failed. Cookie name should not be null.");
+      throw new IllegalArgumentException(COOKIE_NAME_SHOULD_NOT_BE_NULL);
+    }
+    if (this.cookies.containsKey(cookieName)) {
+      String cookieValue = cookies.get(cookieName);
+      this.cookies.remove(cookieName);
+      String cookiesAsString = getCookiesAsString();
+      this.headers.put(COOKIE, cookiesAsString);
+      log.info("Cookie '{}={}' was removed.", cookieName, cookieValue);
+    }
+    log.warn("Removing failed. Cookie for name='{}' not found.", cookieName);
+  }
+
+  private String getCookiesAsString() {
+    List<String> pairs = this.cookies.entrySet().stream()
+        .map(entry -> entry.getKey() + "=" + entry.getValue())
+        .toList();
+    return String.join("; ", pairs);
+  }
+
+  // TODO: add tests for get/set status and get/set body
   public T getBody() {
     log.debug("Get body call");
     return body;
@@ -63,13 +189,37 @@ public class BringResponse<T> {
     this.body = body;
   }
 
-  public Integer getStatus() {
+  public ResponseStatus getStatus() {
     log.debug("Get status call");
     return status;
   }
 
-  public void setStatus(Integer status) {
+  public void setStatus(ResponseStatus status) {
     log.debug("Set status call");
     this.status = status;
+  }
+
+  static class ResponseBuilder<T> {
+    Map<String, String> headers;
+    T body;
+
+    public ResponseBuilder<T> headers(Map<String, String> headers) {
+      this.headers = headers;
+      return this;
+    }
+
+    public ResponseBuilder<T> body(T body) {
+      this.body = body;
+      return this;
+    }
+
+    public BringResponse<T> status(ResponseStatus status) {
+      return new BringResponse<>(body, headers, status);
+    }
+
+    // todo think about termination methods with other response statuses
+    BringResponse<T> ok() {
+      return new BringResponse<>(body, headers, ResponseStatus.OK);
+    }
   }
 }
--- src/test/java/io/github/bobocodebreskul/server/BringResponseTest.java
+++ src/test/java/io/github/bobocodebreskul/server/BringResponseTest.java
@@ -1,39 +1,381 @@
 package io.github.bobocodebreskul.server;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
+
 import io.github.bobocodebreskul.server.enums.ResponseBodyEnum;
+import io.github.bobocodebreskul.server.enums.ResponseStatus;
+import java.lang.reflect.Field;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.params.provider.ValueSource;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BringResponseTest {
 
+  private static final String TEST_HEADER_NAME = "test-header-name";
+  private static final String TEST_HEADER_VALUE = "test-header-value";
+
+  private static final String TEST_COOKIE_NAME = "test-cookie-name";
+  private static final String TEST_COOKIE_VALUE = "test-header-value";
+
+  @SuppressWarnings("unchecked")
   @Order(1)
-  @DisplayName("Check that header is added successfully")
+  @DisplayName("Check that header was added successfully")
   @Test
-  public void given_BringResponse_whenAddHeader_thenHeaderIsSuccessfullyAdded() {
+  void given_BringResponse_When_AddHeader_Then_HeaderIsSuccessfullyAdded()
+      throws NoSuchFieldException, IllegalAccessException {
     // given
     BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
-    String headerName = "testheader";
-    String headerValue = "test-header-value";
 
     // when
-    response.addHeader(headerName, headerValue);
+    response.addHeader(TEST_HEADER_NAME, TEST_HEADER_VALUE);
 
     // then
-    //TODO: finish test logic
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> actual = (Map<String, String>) headers.get(response);
+    assertThat(actual).containsEntry(TEST_HEADER_NAME, TEST_HEADER_VALUE);
+    headers.setAccessible(false);
   }
 
   @Order(2)
-  @DisplayName("Check when ")
+  @DisplayName("Check adding header when header name is null")
   @Test
-  public void given_BringResponse_whenGetNotExistingHeader_then_throwException() {
+  void given_BringResponse_When_AddHeaderWithNullName_Then_ThrowIllegalArgumentException() {
     // given
-    BringResponse<Object> response = new BringResponse<>(new Object());
+    BringResponse<Object> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    Exception actualException =
+        catchException(() -> response.addHeader(null, TEST_HEADER_VALUE));
+
+    // then
+    assertThat(actualException)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(BringResponse.HEADER_NAME_SHOULD_NOT_BE_NULL);
+  }
+
+  @Order(3)
+  @DisplayName("Adding header with null name should throw IllegalArgumentException")
+  @Test
+  void given_BringResponse_When_AddHeaderWithNullValue_Then_ThrowIllegalArgumentException() {
+    // given
+    BringResponse<Object> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    Exception actualException =
+        catchException(() -> response.addHeader(TEST_HEADER_NAME, null));
+
+    // then
+    assertThat(actualException)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(BringResponse.HEADER_VALUE_SHOULD_NOT_BE_NULL);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(4)
+  @DisplayName("Get header successfully")
+  @Test
+  void given_BringResponse_When_GetHeader_Then_ReturnHeaderValue()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> headersMap = (Map<String, String>) headers.get(response);
+    headersMap.put(TEST_HEADER_NAME, TEST_HEADER_VALUE);
+    headers.setAccessible(false);
+
+    // then
+    assertThat(response.getHeader(TEST_HEADER_NAME)).isEqualTo(TEST_HEADER_VALUE);
+  }
+
+  @Order(5)
+  @DisplayName("Get non existing header should return empty string")
+  @Test
+  void given_BringResponse_When_GetNonExistingHeader_Then_ReturnEmptyString() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
 
     // when
 
     // then
-    //TODO: finish test logic
+    assertThat(response.getHeader(TEST_HEADER_NAME)).isEmpty();
   }
 
+  @SuppressWarnings("unchecked")
+  @Order(6)
+  @DisplayName("Check that header is removed successfully")
+  @Test
+  void given_BringResponse_When_RemoveHeader_Then_HeaderIsSuccessfullyRemoved()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> headersMap = (Map<String, String>) headers.get(response);
+    headersMap.put(TEST_HEADER_NAME, TEST_HEADER_VALUE);
+
+    // when
+    response.removeHeader(TEST_HEADER_NAME);
+
+    // then
+    assertThat(headersMap).doesNotContainKey(TEST_HEADER_NAME);
+    headers.setAccessible(false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(7)
+  @DisplayName("Check that cookies was cleaned when header 'Cookie' was removed")
+  @Test
+  void given_BringResponse_When_RemoveHeaderCookie_Then_CleanCookies()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> headersMap = (Map<String, String>) headers.get(response);
+    headersMap.put(BringResponse.COOKIE, TEST_HEADER_VALUE);
+    headers.setAccessible(false);
+
+    // when
+    response.removeHeader(TEST_HEADER_NAME);
+
+    // then
+    Field cookies = response.getClass().getDeclaredField("cookies");
+    cookies.setAccessible(true);
+    Map<String, String> cookiesMap = (Map<String, String>) cookies.get(response);
+    assertThat(cookiesMap).isEmpty();
+    cookies.setAccessible(false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(8)
+  @DisplayName("Check that removing non existing header doesn't removed anything")
+  @Test
+  void given_BringResponse_When_RemoveNonExistingHeader_Then_NotRemoved()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> headersMap = (Map<String, String>) headers.get(response);
+    headersMap.put(TEST_HEADER_NAME, TEST_HEADER_VALUE);
+
+    // when
+    response.removeHeader("TEST-NON-EXIST-HEADER_NAME");
+
+    // then
+    assertThat(headersMap).containsEntry(TEST_HEADER_NAME, TEST_HEADER_VALUE);
+    headers.setAccessible(false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(9)
+  @DisplayName("Check that cookie was added successfully")
+  @Test
+  void given_BringResponse_When_AddCookie_Then_CookieIsSuccessfullyAdded()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    response.addCookie(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+
+    // then
+    Field cookies = response.getClass().getDeclaredField("cookies");
+    cookies.setAccessible(true);
+    Map<String, String> actual = (Map<String, String>) cookies.get(response);
+    assertThat(actual).containsEntry(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+    cookies.setAccessible(false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(10)
+  @DisplayName("Check that header cookie was added successfully")
+  @Test
+  void given_BringResponse_When_AddCookie_Then_CookieIsSuccessfullyAddedToHeaders()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    response.addCookie(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+
+    // then
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> actual = (Map<String, String>) headers.get(response);
+    assertThat(actual)
+        .containsEntry(BringResponse.COOKIE, TEST_COOKIE_NAME + "=" + TEST_COOKIE_VALUE);
+    headers.setAccessible(false);
+  }
+
+  @Order(11)
+  @DisplayName("Adding cookie with null name should throw IllegalArgumentException")
+  @Test
+  void given_BringResponse_When_AddCookieWithNullName_Then_ThrowIllegalArgumentException() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    Exception actualException = catchException(() -> response.addCookie(null, TEST_COOKIE_VALUE));
+
+    // then
+    assertThat(actualException)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(BringResponse.COOKIE_NAME_SHOULD_NOT_BE_NULL);
+  }
+
+  @Order(12)
+  @DisplayName("Adding cookie with null value should throw IllegalArgumentException")
+  @Test
+  void given_BringResponse_When_AddCookieWithNullValue_Then_ThrowIllegalArgumentException() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    Exception actualException = catchException(() -> response.addCookie(TEST_COOKIE_NAME, null));
+
+    // then
+    assertThat(actualException)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(BringResponse.COOKIE_VALUE_SHOULD_NOT_BE_NULL);
+  }
+
+  @Order(13)
+  @DisplayName("Get cookie when exists then return correct value")
+  @Test
+  void given_ExistedValue_When_AddCookieWithNullValue_Then_ReturnValue() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(null, null, ResponseStatus.OK);
+    response.addCookie(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+
+    // when
+    String actual = response.getCookie(TEST_COOKIE_NAME);
+
+    // then
+    assertThat(actual).isEqualTo(TEST_COOKIE_VALUE);
+  }
+
+  @Order(14)
+  @DisplayName("Get cookie when non-exists then return correct value")
+  @Test
+  void given_NonExistedValue_When_AddCookieWithNullValue_Then_ReturnEmpty() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseStatus.OK);
+
+    // when
+    String actualException = response.getCookie(TEST_COOKIE_NAME);
+
+    // then
+    assertThat(actualException).isEmpty();
+  }
+
+  @Order(15)
+  @DisplayName("Get cookie when exists then return correct value")
+  @ValueSource(strings = {"Test-Cookie-Name", TEST_COOKIE_NAME, "TEST-COOKIE-NAME"})
+  @ParameterizedTest
+  void given_DifferentCaseKeys_When_AddCookieWithNullValue_Then_ReturnTheSameValue() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseStatus.OK);
+    response.addCookie(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+
+    // when
+    String actualException = response.getCookie(TEST_COOKIE_NAME);
+
+    // then
+    assertThat(actualException).isEqualTo(TEST_COOKIE_VALUE);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(16)
+  @DisplayName("Check that cookie is removed successfully")
+  @Test
+  void given_BringResponse_When_RemoveCookie_Then_CookieIsSuccessfullyRemoved()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+    Field cookies = response.getClass().getDeclaredField("cookies");
+    cookies.setAccessible(true);
+    Map<String, String> cookiesMap = (Map<String, String>) cookies.get(response);
+    cookiesMap.put(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+
+    // when
+    response.removeCookie(TEST_COOKIE_NAME);
+
+    // then
+    assertThat(cookiesMap).doesNotContainKey(TEST_COOKIE_NAME);
+    cookies.setAccessible(false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(17)
+  @DisplayName("Check that removing non existing header doesn't removed anything")
+  @Test
+  void given_BringResponse_When_RemoveNonExistingCookie_Then_NotRemoved()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+    Field cookies = response.getClass().getDeclaredField("cookies");
+    cookies.setAccessible(true);
+    Map<String, String> cookiesMap = (Map<String, String>) cookies.get(response);
+    cookiesMap.put(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+
+    // when
+    response.removeCookie("TEST-NON-EXIST-HEADER_NAME");
+
+    // then
+    assertThat(cookiesMap).containsEntry(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+    cookies.setAccessible(false);
+  }
+
+  @Order(18)
+  @DisplayName("Adding cookie with null value should throw IllegalArgumentException")
+  @Test
+  void given_BringResponse_When_RemoveCookieWithNullName_Then_ThrowIllegalArgumentException() {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+
+    // when
+    Exception actualException = catchException(() -> response.removeCookie(null));
+
+    // then
+    assertThat(actualException)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(BringResponse.COOKIE_NAME_SHOULD_NOT_BE_NULL);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Order(19)
+  @DisplayName("Check that removing cookie should remove it from headers")
+  @Test
+  void given_BringResponse_When_RemoveCookie_Then_RemovedCookieFromHeaders()
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
+    BringResponse<ResponseBodyEnum> response = new BringResponse<>(ResponseBodyEnum.NONE);
+    response.addCookie(TEST_COOKIE_NAME, TEST_COOKIE_VALUE);
+    response.addCookie("test-cookie-name-2", "test-cookie-value-2");
+
+    // when
+    response.removeCookie("test-cookie-name-2");
+
+    // then
+    Field headers = response.getClass().getDeclaredField("headers");
+    headers.setAccessible(true);
+    Map<String, String> actual = (Map<String, String>) headers.get(response);
+    assertThat(actual)
+        .containsEntry(BringResponse.COOKIE, TEST_COOKIE_NAME + "=" + TEST_COOKIE_VALUE);
+    headers.setAccessible(false);
+  }
+
+  // TODO: add tests for get/set status and get/set body
 }

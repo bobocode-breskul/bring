12eb055acb596e000173f8248961f794f3ce5609
mykola.filimonov <filimonovkolya97@gmail.com>
2023-11-29 23:15:32 +0200

    javadoc

--- src/main/java/io/github/bobocodebreskul/context/annotations/RequestBody.java
+++ src/main/java/io/github/bobocodebreskul/context/annotations/RequestBody.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Applied to method parameters in controller method which specified with RequestMapping.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface RequestBody {

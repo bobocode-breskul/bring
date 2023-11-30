2dc7931ff4c44e948ba8af3cd1b236c3db4a8700
Oleksandr.Karpachov <xalexandrx9@gmail.com>
2023-11-30 21:48:26 +0200

    add java docs

--- src/main/java/io/github/bobocodebreskul/server/BringResponse.java
+++ src/main/java/io/github/bobocodebreskul/server/BringResponse.java
@@ -10,8 +10,20 @@ import javax.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * todo write java doc
- * @param <T>
+ * Represents an HTTP response entity. Gives access to response headers, cookies, body and status.
+ * Could be used as return value in {@code @RestController} methods.
+ * <p>Example:</p>
+ * <pre class="code">
+ *   &#64;Get("/request")
+ *   public BringResponse&lt;String&gt; handleRequest() {
+ *     Map&lt;String, String&gt; headers = new HashMap&lt;&gt;();
+ *     headers.put("Content-Type", "text/html");
+ *     return BringResponse.status(ResponseStatus.CREATED)
+ *       .headers(headers)
+ *       .body("RESULT DATA");
+ *   }
+ * </pre>
+ * @param <T> body type
  */
 @Slf4j
 public class BringResponse<T> {
@@ -45,6 +57,33 @@ public class BringResponse<T> {
       throw new IllegalArgumentException("HttpStatus must not be null");
     }
     this.status = status;
+  }
+
+  /**
+   * Create a builder with specified status
+   * @param status HTTP response status
+   * @return builder instance
+   */
+  public static ResponseBuilder status(ResponseStatus status) {
+    return new ResponseBuilder(status);
+  }
+
+  /**
+   * Create a builder with the status set to {@link ResponseStatus#OK OK}.
+   * @return the created builder
+   */
+  public static ResponseBuilder ok() {
+    return new ResponseBuilder(ResponseStatus.OK);
+  }
+
+  /**
+   * A shortcut for creation of a {@link BringResponse} with {@link ResponseStatus#OK OK}.
+   * @param body the response body.
+   * @return the created {@link BringResponse} object.
+   * @param <T> body class type.
+   */
+  public static <T> BringResponse<T> ok(T body) {
+    return ok().body(body);
   }
 
   /**
@@ -208,22 +247,18 @@ public class BringResponse<T> {
     this.status = status;
   }
 
-  static class ResponseBuilder<T> {
-    Map<String, String> headers;
-    T body;
+  public static class ResponseBuilder {
 
-    public ResponseBuilder<T> headers(Map<String, String> headers) {
+    private final ResponseStatus responseStatus;
+    private Map<String, String> headers;
+
+    private ResponseBuilder(ResponseStatus status) {
+      this.responseStatus = status;
+    }
+
+    public ResponseBuilder headers(Map<String, String> headers) {
       this.headers = headers;
       return this;
-    }
-
-    public ResponseBuilder<T> body(T body) {
-      this.body = body;
-      return this;
-    }
-
-    public BringResponse<T> status(ResponseStatus status) {
-      return new BringResponse<>(body, headers, status);
     }
 
     public <T> BringResponse<T> body(T body) {
@@ -234,4 +269,5 @@ public class BringResponse<T> {
       return new BringResponse<>(null, headers, responseStatus);
     }
   }
+
 }

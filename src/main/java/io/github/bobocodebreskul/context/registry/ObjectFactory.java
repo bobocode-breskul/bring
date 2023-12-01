805cb0bca76abc0667fc96b97c310f1c8239e79d
IIUPAMET <35329349+IIUPAMET@users.noreply.github.com>
2023-12-01 23:43:24 +0200

    added get bean definition by type (#115)

--- src/main/java/io/github/bobocodebreskul/context/registry/ObjectFactory.java
+++ src/main/java/io/github/bobocodebreskul/context/registry/ObjectFactory.java
@@ -1,5 +1,7 @@
 package io.github.bobocodebreskul.context.registry;
 
+import java.util.List;
+
 /**
  * A factory interface for creating and obtaining instances of beans.
  */
@@ -12,5 +14,21 @@ public interface ObjectFactory {
    * @return the instance of the requested bean
    */
   Object getBean(String name);
+
+  /**
+   * Retrieves a bean of the specified type from the container.
+   *
+   * @param clazz the class type of the bean to retrieve
+   * @param <T>   the type of the bean
+   * @return the bean instance of the specified type, or {@code null} if not found
+   */
+  <T> T getBean(Class<T> clazz);
+
+  /**
+   * Retrieves a list of all beans in the container.
+   *
+   * @return a list containing all beans in the container
+   */
+  List<Object> getAllBeans();
 }
 
